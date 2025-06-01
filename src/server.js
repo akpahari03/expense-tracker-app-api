4c63de08be3cc83c32c3ce222525b3830c8eceb3
Ayush Kumar Pahari <ayush@Ayushs-MacBook-Air.local>
2025-06-01 20:22:43 +0530

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,16 +3,23 @@ import dotenv from 'dotenv';
 import { initDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionsRoute from './routes/transactionsRoute.js';
-
+import job from './config/cron.js';
 dotenv.config()
+
 const app = express()
+if(process.env.NODE_ENV === 'production') {
+    job.start()
+}
 const PORT = process.env.PORT || 5001
 
 //middleware
 app.use(rateLimiter)
-app.use(express.json());
 
 //routes
+app.use('/api/health', (req, res) => {
+    res.status(200).json({status: "OK", message: "Server is running smoothly!"});
+})
+
 app.use('/api/transactions',transactionsRoute)
 
 initDB().then(()=> {
